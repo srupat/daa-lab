71ef2a8bab630f9142af42615c95cf673cc4738a
Srujan Patwardhan <srujanpatwardhan@gmail.com>
2025-01-20 13:57:26 +0530

    lab 2 quick sort and merge sort

---
+++ lab_2.py
@@ -0,0 +1,82 @@
+class Node():
+    def __init__(self, val = 0, left = None, right = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        
+def insert(root, key):
+    if root is None:
+        return Node(key)
+    if root.val == key:
+        return root
+    if root.val < key:
+        root.right = insert(root.right, key)
+    else:
+        root.left = insert(root.left, key)
+    return root
+
+def inorder(root):
+    if root:
+        inorder(root.left)
+        print(root.val, end=" ")
+        inorder(root.right)
+        
+def quicksort_bst(l):
+    root = Node(l[0])
+    for i in range(1, len(l)):
+        insert(root, l[i])
+    inorder(root)
+
+def partition(L, lower, upper):
+    pivot = L[lower]
+    i = lower
+    for j in range(lower + 1, upper + 1):
+        if L[j] <= pivot:
+            i += 1
+            L[j], L[i] = L[i], L[j]
+    L[lower], L[i] = L[i], L[lower]
+    return i    
+
+def quicksort(L, lower, upper):
+    if lower < upper:
+        pivot_pos = partition(L, lower, upper)
+        quicksort(L,lower,pivot_pos-1)
+        quicksort(L,pivot_pos+1,upper)
+    return L
+    
+def merge(A, B):
+    m, n = len(A), len(B)
+    C, i, j = [], 0, 0
+    
+    while i < m and j < n:
+        if A[i] <= B[j]:
+            C.append(A[i])
+            i += 1
+        else:
+            C.append(B[j])
+            j += 1
+            
+    while i < m:
+        C.append(A[i])
+        i += 1
+    
+    while j < n:
+        C.append(B[j])
+        j += 1
+        
+    return C
+
+def mergesort(l):
+    n = len(l)
+    if n <= 1:
+        return l
+    m = n // 2
+    left = mergesort(l[:m])
+    right = mergesort(l[m:])
+    sorted = merge(left, right)
+    return sorted
+    
+l = [7, 5, 3, 10, 8, 2, 9]
+quicksort_bst(l)
+print(quicksort(l,0,len(l)-2))
+print(mergesort(l))
