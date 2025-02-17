6046cc69be5bab96b78f40106e4ff4dc059c1a02
Srujan Patwardhan <srujanpatwardhan@gmail.com>
2025-02-17 13:55:34 +0530

    fixed josephus problem

--- lab_3.py
+++ lab_3.py
@@ -7,12 +7,12 @@ def josephus_circ(l, ind, k):
     josephus_circ(l, ind, k)
     
 def josephus_norm(l, k):
-    while(len(l) > 1):
-        for i in range(len(l)):
-            if i % k == 0:
-                l.pop(i)
-    print(l[0])
-    
+    ind = 0 
+    while len(l) > 1:
+        ind = (ind + k) % len(l) 
+        l.pop(ind) 
+    print(l[0])  
+
 n = int(input("Enter the number of people : "))
 l = [i for i in range(1, n + 1)]
 josephus_circ(l, 0, 1)
